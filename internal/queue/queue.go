40142c9a72fd82edb045f1dc563f7070ff00a2bd
Bharanidharan <bharanidharankpi@gmail.com>
2026-03-11 18:07:36 +0530

    Add topic wise queues and simple tests to the queue package

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,6 +1,16 @@
 package queue
 
-import "github.com/Bharanidharan2006/cabbageMQ/internal/core"
+import (
+	"errors"
+	"sync"
+
+	"github.com/Bharanidharan2006/cabbageMQ/internal/core"
+)
+
+type QueueManager struct {
+	queues map[string]*RingBuffer[*core.Job]
+	mu sync.RWMutex
+}
 
 type TopicConfig struct {
 	Topic string
@@ -8,11 +18,67 @@ type TopicConfig struct {
 	MaxBufferSize int
 }
 
-var QueuesMap map[string]*RingBuffer[core.Job] = make(map[string]*RingBuffer[core.Job])
 
-func CreateQueues(configs []TopicConfig) {
-	for _, config := range configs {
-		QueuesMap[config.Topic] = NewRingBuffer[core.Job](config.Size, config.MaxBufferSize)
+func NewQueueManager() *QueueManager {
+	return &QueueManager{
+		queues: make(map[string]*RingBuffer[*core.Job]),
 	}
+}
+
+func (qm *QueueManager)CreateQueues(configs []TopicConfig) {
+	qm.mu.Lock()
+	defer qm.mu.Unlock()
+	for _, config := range configs {
+		qm.queues[config.Topic] = NewRingBuffer[*core.Job](config.Size, config.MaxBufferSize)
+	}
+}
+
+func (qm *QueueManager)CreateQueue(config TopicConfig) {
+	qm.mu.Lock()
+	defer qm.mu.Unlock()
+	qm.queues[config.Topic] = NewRingBuffer[*core.Job](config.Size, config.MaxBufferSize)
+}
+
+func (qm *QueueManager)Push(topic string, job *core.Job) error {
+	qm.mu.Lock()
+	defer qm.mu.Unlock()
+
+	tqueue := qm.queues[topic]
+
+	if tqueue == nil {
+		return errors.New("The queue for the given topic doesn't exist. Create a Queue First");
+	}
+
+	if err := tqueue.Enqueue(job); err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+// Here the bool indicates whether there is some job to read.
+// If it is false, it means there is nothing to read and the queue is empty
+
+
+// The ok parameter is false also when we encounter an error. So in the calling function we first check whether there is an error
+// and only then we check whether the queue is empty denoted by the ok parameter
+
+func (qm *QueueManager)Read(topic string) (*core.Job, bool, error) {
+	qm.mu.RLock()
+	defer qm.mu.RUnlock()
+
+	tqueue := qm.queues[topic]
+
+	if tqueue == nil {
+		return nil, false, errors.New("The queue for the given topic doesn't exist. Create a Queue First");
+	}	
+
+	data, ok := tqueue.Dequeue()
+
+	if !ok {
+		return nil, false, nil
+	}
+
+	return data, true, nil
 }
 
---
+++ internal/queue/queue_test.go
@@ -0,0 +1,51 @@
+package queue
+
+import (
+	"testing"
+
+	"github.com/Bharanidharan2006/cabbageMQ/internal/core"
+)
+
+func TestQueue(t *testing.T) {
+	queueManager := NewQueueManager()
+
+	if queueManager == nil {
+		t.Fatalf("Queue Manager cannot be created!")
+	}
+
+	queueManager.CreateQueue(TopicConfig{
+		Topic: "email",
+		Size: 5,
+		MaxBufferSize: 100,
+	})
+
+	err := queueManager.Push("email", &core.Job{
+		ID: 5,
+	})
+
+	if err != nil {
+		t.Errorf("Cannot push a job into the queue")
+	}
+
+	job, ok, err := queueManager.Read("email") 
+
+	if err != nil && !ok {
+		t.Errorf("Cannot push a job into the queue")
+	}
+
+	if job.ID != 5 {
+		t.Errorf("Job is not retrieved properly")
+	}
+
+	job, ok, err = queueManager.Read("email")
+
+	if err != nil {
+		t.Errorf("Queue should be empty, but error returned")
+	}
+
+	if ok {
+		t.Errorf("Queue should be empty but ok = true is returned")
+	}
+
+
+}
